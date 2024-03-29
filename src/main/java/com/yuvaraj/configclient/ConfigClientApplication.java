b3fd48c362286c11576945a68bfab3da65b7c684
yuvarajengineer <yuvarajengineer@gmail.com>
2024-03-29 11:54:33 +0530

    set up

--- src/main/java/com/yuvaraj/configclient/ConfigClientApplication.java
+++ src/main/java/com/yuvaraj/configclient/ConfigClientApplication.java
@@ -3,11 +3,24 @@ package com.yuvaraj.configclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class ConfigClientApplication {
+
+    @Value("${owner.name}")
+    private String ownerName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
+	
+    @GetMapping("/owner")
+    public String getOwner() {
+        return "Owner Name: " + owner;
+    }
 
 }
